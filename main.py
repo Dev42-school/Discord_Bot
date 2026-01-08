ae2974dd9218d98533931c660114b5ad0d352ea3
thedev42 <the.official.mainframe@gmail.com>
2026-01-08 23:16:02 +0000

    bot

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
+Welcome_channel_id = int(os.getenv('WELCOME_CHANNEL_ID'))
+join_message_id = int(os.getenv('JOIN_MESSAGE_ID'))
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -19,6 +22,25 @@ bot = commands.Bot(command_prefix='>', intents=intents)
 @bot.event
 async def on_ready():
     print(f"bot ready, {bot.user.name}")
+
+
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(Welcome_channel_id)
+    if channel:
+        try:
+            message_to_reply = await channel.fetch_message(join_message_id)
+            await asyncio.sleep(15)
+            sent_message = await channel.send(f"Welcome To The CivCraft Server {member.mention}!", reference=message_to_reply, mention_author=False)
+            await asyncio.sleep(10)
+            await sent_message.delete()
+        except discord.NotFound:
+            await asyncio.sleep(15)
+            sent_message = await channel.send(f"Welcome To The CivCraft Server {member.mention}!")
+            await asyncio.sleep(10)
+            await sent_message.delete()
+
 
 
 @bot.command()
